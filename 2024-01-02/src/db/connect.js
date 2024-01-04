8ead916937229472f9e6d0c620e9edfbfbdd1252
pjhpkm0727 <061116pjh@naver.com>
2024-01-04 09:39:18 +0900

    FEAT: change var

--- 2024-01-02/src/db/connect.js
+++ 2024-01-02/src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.p5ah3dg.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.p5ah3dg.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = async () => {
     await mongoose.connect(connectionString);
