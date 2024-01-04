78849e0b70ddb715960dee7f7e375cc48bbc5b92
pjhpkm0727 <061116pjh@naver.com>
2024-01-04 10:33:07 +0900

    FIX: search user

--- 2024-01-02/src/server/routes/users_me.js
+++ 2024-01-02/src/server/routes/users_me.js
@@ -1,14 +1,14 @@
-const users = require('../../db/users');
+const User = require('../../db/users.schema');
 
 module.exports = {
     path: '/users/me',
     method: 'get',
-    handler: (req, res) => {
-        const { idx } = req.session;
+    handler: async (req, res) => {
+        const { _id } = req.session;
 
-        const me = users.find(user => {
-            return user.idx === idx;
-        });
+        const me = await User.findOne({ _id });
+
+        if (me === null) throw new Error('403: 로그인이 필요합니다.');
 
         return res.json(me);
     }
