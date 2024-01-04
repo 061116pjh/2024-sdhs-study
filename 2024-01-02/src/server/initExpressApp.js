fb640ae6a3b21d367301bbc741db2eb1afc15922
pjhpkm0727 <061116pjh@naver.com>
2024-01-04 12:38:59 +0900

    FEAT: express static file

--- 2024-01-02/src/server/initExpressApp.js
+++ 2024-01-02/src/server/initExpressApp.js
@@ -9,4 +9,6 @@ module.exports = (app) => {
         secret: 'keyboard cat',
         cookie: { secure: false }
     }));
+    app.use(express.json());
+    app.use(express.static('../../static'));
 }
