a69c9d34a84550cee0e0a506ef1d9acbbf5278c0
pjhpkm0727 <061116pjh@naver.com>
2024-01-04 15:35:25 +0900

    FEAT: deletePost

---
+++ 2024-01-02/src/server/routes/posts/deletePost.js
@@ -0,0 +1,14 @@
+const { Types: { ObjectId }} = require('mongoose');
+const Post = require('@db/posts.schema');
+
+module.exports = {
+    path: '/posts/:postId',
+    method: 'delete',
+    handler: async (req, res) => {
+        const { postId: _id } = req.params;
+
+        await Post.deleteOne({ _id: new ObjectId(_id) });
+
+        return res.redirect('/');
+    }
+}
