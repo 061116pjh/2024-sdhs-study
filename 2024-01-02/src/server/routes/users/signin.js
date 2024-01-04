78005e90659fd81b81c2df146d7e91a67531483f
pjhpkm0727 <061116pjh@naver.com>
2024-01-04 12:39:24 +0900

    FIX: idx => _id

--- 2024-01-02/src/server/routes/users/signin.js
+++ 2024-01-02/src/server/routes/users/signin.js
@@ -16,9 +16,9 @@ module.exports = {
 
         const user = await findUserByIdAndPassword();
 
-        if(user === null) throw new Error('401: 아이디와 비밀번호가 일치하지 않습니다.');
+        if(user === null) throw new Error('401:아이디와 비밀번호가 일치하지 않습니다.');
         
-        req.session.idx = user._id;
+        req.session._id = user._id;
 
         return res.json({ success: true });
     }
