53bc1aa5373544e8254984c5ee29364c06e5f2cc
pjhpkm0727 <061116pjh@naver.com>
2024-01-05 09:52:02 +0900

    DOCS: comment

--- 2024-01-02/index.ts
+++ 2024-01-02/index.ts
@@ -1,37 +1,37 @@
-import Express, { Request, Response, Application } from 'express';
+// import Express, { Request, Response, Application } from 'express';
 
-interface Server{
-    start: () => Promise<void>,
-    getRoutes: ( parentRoute: Route) => Route[]
-    getUpTime: () => number,
-}
+// interface Server{
+//     start: () => Promise<void>,
+//     getRoutes: ( parentRoute: Route) => Route[]
+//     getUpTime: () => number,
+// }
 
-class ExpressServer implements Server{
-    private readonly app: Application;
-    private readonly port = process.env.PORT;
+// class ExpressServer implements Server{
+//     private readonly app: Application;
+//     private readonly port = process.env.PORT;
 
-    constructor(){
-        this.app = Express();
-    }
+//     constructor(){
+//         this.app = Express();
+//     }
 
-    async start(){
-        this.app.listen(this.port, () => {
-            console.log('server is running on port: ', this.port);
-        });
-    }
-}
+//     async start(){
+//         this.app.listen(this.port, () => {
+//             console.log('server is running on port: ', this.port);
+//         });
+//     }
+// }
 
-interface Route{
-    path: string,
-    method: 'get' | 'post' | 'put' | 'patch' | 'delete',
-    handler: (req: Request, res: Response) => Promise<void | Response>
-}
+// interface Route{
+//     path: string,
+//     method: 'get' | 'post' | 'put' | 'patch' | 'delete',
+//     handler: (req: Request, res: Response) => Promise<void | Response>
+// }
 
-const signinRoute: Route = {
-    path: '/signin',
-    method: 'post',
-    handler: async (req, res) => {
+// const signinRoute: Route = {
+//     path: '/signin',
+//     method: 'post',
+//     handler: async (req, res) => {
 
-        return res.json();
-    }
-}
+//         return res.json();
+//     }
+// }
