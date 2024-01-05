4fd98e0b2768308aad6cceaba86a187f5f6a278f
pjhpkm0727 <061116pjh@naver.com>
2024-01-05 11:03:20 +0900

    FEAT: function rename

--- 2024-01-02/src/db/connect.ts
+++ 2024-01-02/src/db/connect.ts
@@ -4,7 +4,7 @@ const id: string = process.env.MONGODB_USERNAME as string;
 const pw: string = process.env.MONGODB_PASSWORD as string;
 const connectionString = `mongodb+srv://${id}:${pw}@cluster0.p5ah3dg.mongodb.net/?retryWrites=true&w=majority`;
 
-export const connectDB =  async () => {
+export const dbConnect =  async () => {
     await mongoose.connect(connectionString);
     console.log('Connected!!!');
 }
