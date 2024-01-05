5324b1c567e879e3348d1b6ca527dd7a5e014342
pjhpkm0727 <061116pjh@naver.com>
2024-01-05 10:23:14 +0900

    FEAT: .js => .ts

--- 2024-01-02/src/db/users.schema.ts
+++ 2024-01-02/src/db/users.schema.ts
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
     id: {
@@ -19,4 +19,4 @@ const userSchema = new Schema({
     }
 });
 
-module.exports = model('User', userSchema);
+export default model('User', userSchema);
